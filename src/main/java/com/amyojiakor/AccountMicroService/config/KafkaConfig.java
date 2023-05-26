f5a8d3ea207faa28053381f6d6d6520c8395bc6e
Amarachukwu Ojiakor <amyojiakor@gmail.com>
2023-05-26 02:13:07 +0100

    Added configurations to consume and publish to credit-account and account-service-credit-response topics respectively

--- src/main/java/com/amyojiakor/AccountMicroService/config/KafkaConfig.java
+++ src/main/java/com/amyojiakor/AccountMicroService/config/KafkaConfig.java
@@ -1,13 +1,10 @@
 package com.amyojiakor.AccountMicroService.config;
 
-import com.amyojiakor.AccountMicroService.models.payloads.AccountResponse;
-import com.amyojiakor.AccountMicroService.models.payloads.TransactionMessage;
-import com.amyojiakor.AccountMicroService.models.payloads.TransactionMessageResponse;
+import com.amyojiakor.AccountMicroService.models.payloads.*;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -59,6 +56,20 @@ public class KafkaConfig {
     }
 
     @Bean
+    public ProducerFactory<String, CreditAccountMessageResponse> creditAccountProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, CreditAccountMessageResponse> creditAccountKafkaTemplate() {
+        return new KafkaTemplate<>(creditAccountProducerFactory());
+    }
+
+    @Bean
     public Map<String, Object> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
@@ -75,5 +86,14 @@ public class KafkaConfig {
         factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(consumerFactory(), new StringDeserializer(), new JsonDeserializer<>(TransactionMessage.class, false)));
         return factory;
     }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, CreditAccountMessage> creditAccountListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, CreditAccountMessage> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(new DefaultKafkaConsumerFactory<>(consumerFactory(), new StringDeserializer(), new JsonDeserializer<>(CreditAccountMessage.class, false)));
+        return factory;
+    }
+
 
 }
